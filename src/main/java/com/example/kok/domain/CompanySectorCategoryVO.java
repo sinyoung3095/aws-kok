15b73b1329afffdb68cbfec4cbc8f62243b56778
Eunyu <wjddmsdb03@gmail.com>
2025-09-24 14:10:58 +0900

    VO 완료

--- src/main/java/com/example/kok/domain/CompanySectorCategoryVO.java
+++ src/main/java/com/example/kok/domain/CompanySectorCategoryVO.java
@@ -11,4 +11,6 @@ import lombok.experimental.SuperBuilder;
 @EqualsAndHashCode(of="id", callSuper = false)
 @SuperBuilder
 public class CompanySectorCategoryVO extends Period{
+    private Long companyId;
+    private Long companySectorId;
 }
