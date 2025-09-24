1111035a204b7a41b1e8efa97b6b892621c1ccd9
anna0121222 <anna012188@gmail.com>
2025-09-24 13:33:57 +0900

    MemberVO

--- src/main/java/com/example/kok/domain/MemberStorageFileVO.java
+++ src/main/java/com/example/kok/domain/MemberStorageFileVO.java
@@ -11,4 +11,6 @@ import lombok.experimental.SuperBuilder;
 @EqualsAndHashCode(of="id", callSuper = false)
 @SuperBuilder
 public class MemberStorageFileVO extends Period{
+    private Long fileId;
+    private Long memberId;
 }
