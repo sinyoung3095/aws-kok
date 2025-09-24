aba236c9531b7b0a61037476c60b96bcb05ea6b9
Eunyu <wjddmsdb03@gmail.com>
2025-09-24 13:58:31 +0900

    VO

--- src/main/java/com/example/kok/domain/FollowVO.java
+++ src/main/java/com/example/kok/domain/FollowVO.java
@@ -1,6 +1,7 @@
 package com.example.kok.domain;
 
 import com.example.kok.audit.Period;
+import com.example.kok.enumeration.Status;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.ToString;
@@ -11,4 +12,8 @@ import lombok.experimental.SuperBuilder;
 @EqualsAndHashCode(of="id", callSuper = false)
 @SuperBuilder
 public class FollowVO extends Period{
+    private Long id;
+    private Long memberId;
+    private Long companyId;
+    private Status followStatus;
 }
