4878e4bdb2e8ccd655bdc691d3d37ffadba151da
anna0121222 <anna012188@gmail.com>
2025-09-26 09:28:55 +0900

    0926(2)

--- src/main/java/com/example/kok/service/PostServiceImpl.java
+++ src/main/java/com/example/kok/service/PostServiceImpl.java
@@ -8,27 +8,27 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-//@Service
-//@RequiredArgsConstructor
-//public class PostServiceImpl implements PostService {
-//    @Override
-//    public PostCriteriaDTO getList(int page) {
-//        PostCriteriaDTO postCriteriaDTO = new PostCriteriaDTO();
-//        Criteria criteria = new Criteria(page, postDAO.findCountAll());
-//        List<PostDTO> posts = postDAO.findAll(criteria);
-//        posts.forEach((post) -> {
-//            post.setRelativeDate(DateUtils.toRelativeTime(post.getCreatedDatetime()));
-//        });
-//
-//        criteria.setHasMore(posts.size() > criteria.getRowCount());
-//
-////        11개 가져왔으면, 마지막 1개 삭제
-//        if(criteria.isHasMore()){
-//            posts.remove(posts.size() - 1);
-//        }
-//
-//        postCriteriaDTO.setPosts(posts);
-//        postCriteriaDTO.setCriteria(criteria);
-//        return postCriteriaDTO;
-//    }
-//}
+@Service
+@RequiredArgsConstructor
+public class PostServiceImpl implements PostService {
+    @Override
+    public PostCriteriaDTO getList(int page) {
+        PostCriteriaDTO postCriteriaDTO = new PostCriteriaDTO();
+        Criteria criteria = new Criteria(page, postDAO.findCountAll());
+        List<PostDTO> posts = postDAO.findAll(criteria);
+        posts.forEach((post) -> {
+            post.setRelativeDate(DateUtils.toRelativeTime(post.getCreatedDatetime()));
+        });
+
+        criteria.setHasMore(posts.size() > criteria.getRowCount());
+
+//        11개 가져왔으면, 마지막 1개 삭제
+        if(criteria.isHasMore()){
+            posts.remove(posts.size() - 1);
+        }
+
+        postCriteriaDTO.setPosts(posts);
+        postCriteriaDTO.setCriteria(criteria);
+        return postCriteriaDTO;
+    }
+}
