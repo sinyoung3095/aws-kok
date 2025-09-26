7ab939e55bad2b85a197ebfc177dcc90a8a0b289
anna0121222 <anna012188@gmail.com>
2025-09-26 09:23:21 +0900

    0926

--- src/main/resources/static/js/admin/support/module/layout.js
+++ src/main/resources/static/js/admin/support/module/layout.js
@@ -14,6 +14,30 @@ const layout = (() => {
            `;
         });
         noticeListContainer.innerHTML += text;
+
+        text = ``;
+
+        // const pageContainer = document.getElementById("page-container");
+        // text = ``;
+        //
+        // if(criteria.hasPreviousPage){
+        //    text = `<a href="/post/list/${criteria.startPage - 1}">이전</a>`
+        // }
+        //
+        // for(let i = criteria.startPage; i <= criteria.endPage; i++){
+        //    text += `
+        //       <a href="/post/list/${i}">${i}</a>
+        //    `;
+        // }
+        //
+        // if(criteria.hasNextPage){
+        //    text += `<a href="/post/list/${criteria.endPage + 1}">다음</a>`
+        // }
+        // pageContainer.innerHTML = text;
+
+
+
+
     }
     return {showList: showList};
 })();
