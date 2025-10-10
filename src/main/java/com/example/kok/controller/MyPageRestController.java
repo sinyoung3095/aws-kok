85cf5726a3cbf95e1018cfc969b88e25ca270133
Eunyu <wjddmsdb03@gmail.com>
2025-10-10 15:58:30 +0900

    pr

--- src/main/java/com/example/kok/controller/MyPageRestController.java
+++ src/main/java/com/example/kok/controller/MyPageRestController.java
@@ -1,6 +1,8 @@
 package com.example.kok.controller;
 
 import com.example.kok.auth.CustomUserDetails;
+import com.example.kok.dto.RequestExperienceDTO;
+import com.example.kok.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -8,12 +10,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/mypage/**")
 @RequiredArgsConstructor
 public class MyPageRestController {
-//    @GetMapping("/request-list")
-//    public ResponseEntity<?> getRequests(@AuthenticationPrincipal CustomUserDetails customUserDetails){
-//        long memberId=customUserDetails.getId();
-//    }
+    private final MemberService memberService;
+
+    @GetMapping("/request-list")
+    public ResponseEntity<?> getRequests(@AuthenticationPrincipal CustomUserDetails customUserDetails){
+        long memberId=customUserDetails.getId();
+        List<RequestExperienceDTO> req=memberService.findRequestExperienceByMemberId(memberId);
+        if(req.size()!=0){
+            return ResponseEntity.ok(req);
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
