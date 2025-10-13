b2dfaaccefe0afba985db73d0f071c495a1d8158
anna0121222 <anna012188@gmail.com>
2025-10-14 02:56:19 +0900

    admin/main-page finish

--- src/main/resources/static/js/main-page/side-bar/module/service.js
+++ src/main/resources/static/js/main-page/side-bar/module/service.js
@@ -97,5 +97,42 @@ const service = (()=>{
             throw new Error(errorText || "Fetch error");
         }
     }
-    return {getPopularCompany:getPopularCompany,getExperience:getExperience,getIntern:getIntern,getAlarm:getAlarm,setActive:setActive,setInactive:setInactive}
+
+    const getSupportList = async (callback, page = 1, keyword = '') => {
+        const response = await fetch(`/api/side-bar/support/${page}?keyword=${keyword}`);
+        const adminNoticeCriteriaDTO = await response.json();
+        if(callback){
+            callback(adminNoticeCriteriaDTO);
+        }
+
+        if(response.ok) {
+            console.log("공지 게시글 존재")
+        }else if(response.status === 404){
+            console.log("공지 게시글 없음")
+        }else {
+            const error = await response.text()
+            console.log(error);
+        }
+        return adminNoticeCriteriaDTO;
+    }
+
+    const getSupportDetail = async (callback, id = 1) => {
+        const response = await fetch(`/api/side-bar/support/detail/${id}`);
+        const adminNoticeDTO = await response.json();
+        if(callback){
+            callback(adminNoticeDTO);
+        }
+
+        if(response.ok) {
+            console.log("공지 상세 게시글 존재")
+        }else if(response.status === 404){
+            console.log("공지 상세 게시글 없음")
+        }else {
+            const error = await response.text()
+            console.log(error);
+        }
+        return adminNoticeDTO;
+    }
+
+    return {getPopularCompany:getPopularCompany,getExperience:getExperience,getIntern:getIntern,getAlarm:getAlarm,setActive:setActive,setInactive:setInactive,getSupportList:getSupportList,getSupportDetail:getSupportDetail}
 })();
